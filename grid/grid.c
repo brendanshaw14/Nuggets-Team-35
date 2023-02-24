027a98d0b5314bbdf912c7bbe7f5f1123f34f3c9
Brendan N. Shaw <f005670@plank.thayer.dartmouth.edu>
2023-02-24 16:56:20 -0500

    Created grid.c and grid.h

---
+++ grid/grid.c
@@ -0,0 +1,21 @@
+/* Grid.c - the grid module for the CS50 nuggets project
+Authors: Brendan Shaw, Romeo Myrthil, Ming Cheng
+CS50- Winter 2023
+See grid.h for detailed info.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//write the hashtable include here:  
+
+/****************global types**************/
+typedef struct grid {
+    char* grid;
+    hashtable_t* playerTable;
+    const int numRows;
+    const int numColumns;
+
+} grid_t;
+
+
+
---
+++ grid/grid.h
@@ -0,0 +1,6 @@
+/* Grid.h - the grid module for the CS50 nuggets project
+Authors: Brendan Shaw, Romeo Myrthil, Ming Cheng
+CS50- Winter 2023*/
+
+
+
